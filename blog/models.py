411e07698d7ff3b4da6736f478b8c4ca026fa114
teiny <teiny.4l@gmail.com>
2025-01-04 15:45:18 +0500

    Все русифицировал и исправил мелкие ошибки

--- blog/models.py
+++ blog/models.py
@@ -7,11 +7,12 @@ from django.utils import timezone
 class Post(models.Model):  # Объявление новой модели Post(наследуется от класса Model Django)
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)  # Настройки user-а из ForeignKey.
     # on_delete=models.CASCADE при удалении user-a, связанного с этим сообщением все его сообщения также будут удалены.
-    title = models.CharField(max_length=200)  # Заголовок = наследуется символьное поле(НЕ БОЛЬШЕ 200).
-    text = models.TextField(max_length=700)  # Текст = наследуется текстовое поле(НЕ БОЛЬШЕ 700).
+    title = models.CharField(verbose_name="Введите заголовок:", max_length=200)  # Символьное поле(НЕ БОЛЬШЕ 200).
+    text = models.TextField(verbose_name="Введите текст поста:", max_length=700)  # Текстовое поле(НЕ БОЛЬШЕ 700).
     created_date = models.DateTimeField(default=timezone.now)  # Наследуется из models. Поле даты и
     # времени(устанавливает значение поля по умолчанию на текущую дату и время при создании публикации).
-    published_date = models.DateTimeField(blank=True, null=True)  # blank=True: в формах поле может быть пустым.
+    published_date = models.DateTimeField(blank=True, null=True)
+    # Blank=True: в формах поле может быть пустым.
     # Null=True: в базе данных поле может быть пустым. (Означает, что публикация может существовать в
     # черновике и не быть опубликованной).
 
@@ -28,15 +29,15 @@ class Comment(models.Model):  # Объявление новой модели Com
     post = models.ForeignKey("Post", on_delete=models.CASCADE, related_name="comments")  # С помощью FK связываем
     # C моделью Post (если она существует). on_delete=models.CASCADE: Как я понял если Post удаляется, то и комменты.
     # related_name="comments" Тобиш покажет все связанные комментарии в публикации.
-    author_name = models.CharField(verbose_name="Автор комментария", max_length=100)  # Поле автора в 100 символов.
-    text = models.TextField(verbose_name="Текс комментария", max_length=700)  # Текст комментария.
+    author_name = models.CharField(verbose_name="Введите ваше имя:", max_length=100)  # Поле автора в 100 символов.
+    text = models.TextField(verbose_name="Введите текст:", max_length=700)  # Текст комментария.
     created_date = models.DateTimeField(default=timezone.now)  # DateTimeField команда сохраняет как дату, так и время.
     # default=timezone.now: Тобиш по умолчанию равно времени создания.
-    is_deleted = models.BooleanField(verbose_name="Видимость статьи", default=False, null=True)  # Флаг для удаления.
+    is_deleted = models.BooleanField(verbose_name="Видимость коммента", default=False, null=True)  # Флаг для удаления.
 
     def __str__(self):  # То что отображается
         author_display = self.author_name if self.author_name else "Аноним"  # Если имя не указано, выставить Аноним.
-        return f"Комментарий {author_display} в '{self.post.title}': {self.text[:50]}..."  # Сократил для удобства.
+        return f"{author_display}:'{self.post.title}':{self.text[:50]}..."
 
 
 class Like(models.Model):
